051f5d73225cb30ae1c3ce8880cdcfdb0ebbd9f4
vivek <hello@gmail.com>
2021-03-26 18:29:08 +0530

    rename commit(1)

--- Section3/Section3-Homework-Data.R
+++ Section3/Section3-Homework-Data.R
@@ -1,5 +1,70 @@
 #Data
 revenue <- c(14574.49, 7606.46, 8611.41, 9175.41, 8058.65, 8105.44, 11496.28, 9766.09, 10305.32, 14379.96, 10713.97, 15433.50)
 expenses <- c(12051.82, 5695.07, 12319.20, 12089.72, 8658.57, 840.20, 3285.73, 5821.12, 6976.93, 16618.61, 10054.37, 3803.96)
-
+is.double(revenue)
 #Solution
+#profit for each month
+profit=revenue-expenses
+profit  
+
+# tax for each month
+tax=round((0.30)*profit,2)
+tax
+
+#profit after tax for each month
+profit_after_tax=profit-tax
+profit_after_tax
+
+#profit margin for each month
+profit_margin=round(profit_after_tax/revenue,2)*100
+profit_margin
+
+#mean
+profit_mean=mean(profit_after_tax)
+profit_mean
+
+#good months
+good_months<-profit_after_tax>profit_mean
+good_months
+
+#bad months
+bad_months<-!good_months
+bad_months
+
+#Best month
+Best_month<-profit_after_tax==max(profit_after_tax)
+Best_month
+
+worst_month<-profit_after_tax==min(profit_after_tax)
+worst_month
+
+#units of thousands
+expenses_1000<-round(expenses/1000,0)
+revenue_1000<-round(revenue/1000)
+profit_1000<-round(profit/1000)
+profit_after_tax_1000<-round(profit_after_tax/1000)
+
+#output
+revenue_1000
+expenses_1000
+profit_1000
+profit_after_tax_1000
+profit_margin
+good_months
+bad_months
+Best_month
+worst_month
+
+#Matrices
+m<-rbind(
+  revenue_1000,
+  expenses_1000,
+  profit_1000,
+  profit_after_tax_1000,
+  profit_margin,
+  good_months,
+  bad_months,
+  Best_month,
+  worst_month
+)
+m
